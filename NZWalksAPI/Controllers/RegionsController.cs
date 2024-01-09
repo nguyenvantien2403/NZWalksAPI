890a6612e623e6bca01be7acd654c4df80b0dc95
nguyenvantien2403 <88313441+nguyenvantien2403@users.noreply.github.com>
2024-01-09 18:24:34 +0700

    first commit

---
+++ NZWalksAPI/Controllers/RegionsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using NZWalksAPI.Models.Domain;
+
+namespace NZWalksAPI.Controllers
+{
+    ///https://localhost:7061/api/regions             khi url gọi đến tên sau gạch chéo regions sẽ tương ứng với bộ điều khiển RegionsController và phương thức bên dưới sẽ hoạt động
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        [HttpGet] //đang tạo phương thức lấy tất cả nên sẽ dùng httpget
+        public IActionResult GetAll()
+        {
+            var regions = new List<Region>
+            {
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Name",
+                    Code = "123",
+                    RegionImageUrl ="fsdfgsfgsgsgsdg"
+                }
+            };
+            return Ok(regions);
+    
+
+        }
+    }
+}
